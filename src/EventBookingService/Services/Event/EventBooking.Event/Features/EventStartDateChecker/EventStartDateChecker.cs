d7894273ffa56654e63c02d085c840a0d97d56d8
Apolos666 <quangprox121@gmail.com>
2024-09-12 10:33:20 +0700

    feat: Complete EventStartDateNotification function using BackgroundService, RabbitMQ, and SignalR to notify users 2

--- src/EventBookingService/Services/Event/EventBooking.Event/Features/EventStartDateChecker/EventStartDateChecker.cs
+++ src/EventBookingService/Services/Event/EventBooking.Event/Features/EventStartDateChecker/EventStartDateChecker.cs
@@ -5,17 +5,26 @@ public class EventStartDateChecker(
     IServiceScopeFactory serviceScopeFactory)
     : BackgroundService
 {
+    // Prefix for cache keys to prevent duplicate notifications
+    private const string EVENT_NOTIFICATION_PREFIX = "event_being_notificated_";
+    // Time interval between each check for upcoming events
     private static readonly TimeSpan _period = TimeSpan.FromMinutes(1);
+    // Time window to check for events that are about to start
     private static readonly TimeSpan _eventCheckWindow = TimeSpan.FromMinutes(5);
-
+    // Time duration to prevent duplicate notifications
+    private static readonly TimeSpan _preventDuplicateNotification = TimeSpan.FromMinutes(6);
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         using PeriodicTimer timer = new(_period);
 
         while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
         {
+            logger.LogInformation("UtcNow: {UtcNow}", DateTimeOffset.UtcNow);
+
             try
             {
+                // Check and notify upcoming events
                 await CheckAndNotifyUpcomingEvents(stoppingToken);
             }
             catch (Exception ex)
@@ -25,6 +34,7 @@ public class EventStartDateChecker(
         }
     }
 
+    // Checks for events that are about to start and sends notifications
     private async Task CheckAndNotifyUpcomingEvents(CancellationToken stoppingToken)
     {
         logger.LogInformation("Checking for events starting in the next {Minutes} minutes...",
@@ -33,30 +43,53 @@ public class EventStartDateChecker(
         using var scope = serviceScopeFactory.CreateScope();
         var session = scope.ServiceProvider.GetRequiredService<IDocumentSession>();
         var publishEndpoint = scope.ServiceProvider.GetRequiredService<IPublishEndpoint>();
+        var cache = scope.ServiceProvider.GetRequiredService<IDistributedCache>();
 
-        var now = DateTimeOffset.UtcNow;
-        var eventAboutToStart = await GetUpcomingEvents(session, now, stoppingToken);
+        // Retrieve upcoming events
+        var eventsAboutToStart = await GetUpcomingEvents(session, stoppingToken);
 
-        logger.LogInformation("Found {Count} events starting in the next {Minutes} minutes", eventAboutToStart.Count,
+        logger.LogInformation("Found {Count} events starting in the next {Minutes} minutes", eventsAboutToStart.Count,
             _eventCheckWindow.TotalMinutes);
 
-        if (eventAboutToStart.Any())
-        {
-            var startingSoonEvents = eventAboutToStart.Select(CreateStartingSoonEvent);
-            await publishEndpoint.Publish(startingSoonEvents, stoppingToken);
-        }
+        if (!eventsAboutToStart.Any()) return;
+
+        // Send notifications for each event
+        var notificationTasks = eventsAboutToStart.Select(e => NotifyEvent(e, publishEndpoint, cache, stoppingToken));
+        await Task.WhenAll(notificationTasks);
     }
 
-    private static async Task<IReadOnlyList<Models.Event>> GetUpcomingEvents(IDocumentSession session, DateTimeOffset now, CancellationToken stoppingToken)
+    // Sends a notification for a specific event if it has not already been notified
+    private async Task NotifyEvent(Models.Event @event, IPublishEndpoint publishEndpoint, IDistributedCache cache, CancellationToken stoppingToken)
     {
-        var fiveMinutesFromNow = now.AddMinutes(5);
-        var tenMinutesFromNow = now.AddMinutes(10);
+        var cacheKey = $"{EVENT_NOTIFICATION_PREFIX}{@event.Id}";
+        var isNotified = await cache.GetStringAsync(cacheKey, stoppingToken);
+        if (!string.IsNullOrWhiteSpace(isNotified))
+        {
+            logger.LogInformation("Event {EventId} has already been notified", @event.Id);
+            return;
+        }
+
+        var startingSoonEvent = CreateStartingSoonEvent(@event);
+        await publishEndpoint.Publish(startingSoonEvent, stoppingToken);
+        logger.LogInformation("Notification sent for event: {EventId}", @event.Id);
+
+        await cache.SetStringAsync(cacheKey, "true", new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = _preventDuplicateNotification
+        }, token: stoppingToken);
+    }
+
+    // Retrieves events that are about to start within the specified time window
+    private static async Task<IReadOnlyList<Models.Event>> GetUpcomingEvents(IDocumentSession session, CancellationToken stoppingToken)
+    {
+        var now = DateTimeOffset.UtcNow;
+        var endCheckWindow = now.Add(_eventCheckWindow);
 
         return await session.Query<Models.Event>()
-            .Where(e => e.StartDateTime >= fiveMinutesFromNow && e.StartDateTime <= tenMinutesFromNow)
+            .Where(e => e.StartDateTime >= now && e.StartDateTime <= endCheckWindow)
             .ToListAsync(token: stoppingToken);
     }
-
+    
     private static StartingSoonEvent CreateStartingSoonEvent(Models.Event e) =>
         new()
         {
---
+++ src/EventBookingService/Services/Notification/EventBooking.Notification/Features/StartingSoonEventHandler/StartingSoonEventHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace EventBooking.Notification.Features.StartingSoonEventHandler;
+
+public class StartingSoonEventHandler
+    (ILogger<StartingSoonEventHandler> logger, IHubContext<NotificationHub, INotificationClient> hub)
+    : IConsumer<StartingSoonEvent>
+{
+    public async Task Consume(ConsumeContext<StartingSoonEvent> context)
+    {
+        logger.LogInformation("Sending notification to user {UserId} for event {Name}", context.Message.UserId, context.Message.Name);
+        
+        const string title = "Event starting soon";
+        var message = $"{context.Message.Name} about starting in {context.Message.StartDateTime.Millisecond}/{context.Message.StartDateTime.Minute}/{context.Message.StartDateTime.Hour}";
+
+        var userIds = context.Message.UserId.Select(ui => ui.ToString()).ToList();
+        await hub.Clients.Users(userIds).ReceiveNotification(title, message);
+    }
+}
